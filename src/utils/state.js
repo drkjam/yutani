a2b8785da366f0f2805644177639b0a7fcc5172b
David Moss <drkjam@gmail.com>
2017-06-16 13:00:03 +0100

    - changed logging for state transitions

--- src/utils/state.js
+++ src/utils/state.js
@@ -11,8 +11,8 @@ class State {
         if (this.states.hasOwnProperty(new_state)) {
             let next_states = this.states[this.current]
             if (next_states.indexOf(new_state) !== -1) {
+                console.log("state: " + this.current + ' -> ' + new_state)
                 this.current = new_state
-                console.log("state transition: " + this.current)
             }
         }
     }
